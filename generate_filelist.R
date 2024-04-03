0dc7b9c3f5041456608276a599a978c3875ec6f9
yshao <yshao@vt.edu>
2024-04-03 19:41:59 -0400

    added an Rscript for TIMESAT

--- generate_filelist.R
+++ generate_filelist.R
@@ -9,11 +9,14 @@ f1 <- file(outfile, "w")
 # Write the initial line; We have a total of 69 MODIS NDVI files. 
 writeLines("69", f1)
 
-# Loop to generate file paths and write to the files; Revise file path please. 
-for (i in 1:69) {
+# Loop to generate file paths and write to the files; Note we
+#only use the first year (2021) for this lab; 
+for (j in 1:3){
+for (i in 1:23) {
   line <- paste(filepath,"enviformat/MODIS",i,'.dat', sep="")
   print(line)
-   writeLines(line, f1)
+  writeLines(line, f1)
+}
 }
 
 # Close the file connections
